84f9bcf08bfaf7ce6392f468aec64e2e97cd5a84
Kapil Sharma <kapilsharmanfl97@gmail.com>
2022-12-26 00:15:54 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
